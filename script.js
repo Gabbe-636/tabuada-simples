857c4387219828c73c16879757fe9036cca79aa4
Gabriel F. K <112198308+Gabbe-636@users.noreply.github.com>
2022-11-21 21:41:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+// 1) Crie um programa que dado um número imprima a sua tabuada.
+const tab1 = 1;
+for (let i = 1; i <= 10; i++) {
+  console.log(i * tab1);
+}
+console.log("Fim da Tabuada do 1");
+const tab2 = 2;
+for (let i2 = 1; i2 <= 10; i2++) {
+  console.log(i2 * tab2);
+}
+console.log("Fim da Tabuada do 2");
+
+const tab3 = 3;
+for (let i3 = 1; i3 <= 10; i3++) {
+  console.log(i3 * tab3);
+}
+console.log("Fim da Tabuada do 3");
+
+const tab4 = 4;
+for (let i4 = 1; i4 <= 10; i4++) {
+  console.log(i4 * tab4);
+}
+console.log("Fim da Tabuada do 4");
+const tab5 = 5;
+for (let i5 = 1; i5 <= 10; i5++) {
+  console.log(i5 * tab5);
+}
+console.log("Fim da Tabuada do 5");
+
+const tab6 = 6;
+for (let i6 = 1; i6 <= 10; i6++) {
+  console.log(i6 * tab6);
+}
+console.log("Fim da Tabuada do 6");
+
+const tab7 = 7;
+for (let i7 = 1; i7 <= 10; i7++) {
+  console.log(i7 * tab7);
+}
+console.log("Fim da Tabuada do 7");
+
+const tab8 = 8;
+for (let i8 = 1; i8 <= 10; i8++) {
+  console.log(i8 * tab8);
+}
+console.log("Fim da Tabuada do 8");
+
+const tab9 = 9;
+for (let i9 = 1; i9 <= 10; i9++) {
+  console.log(i9 * tab9);
+}
+console.log("Fim da Tabuada do 9");
+
+const tab10 = 10;
+for (let i10 = 1; i10 <= 10; i10++) {
+  console.log(i10 * tab10);
+}
+console.log("Fim da Tabuada do 10");
